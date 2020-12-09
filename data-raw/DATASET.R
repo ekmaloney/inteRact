8e24751791b79fdad29892b00036b30bedc704eb
Emily Maloney <emily.k.maloney@duke.edu>
2020-12-09 12:06:58 -0500

    fixing and documenting data

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -8,7 +8,7 @@ us_2015_mod <- read_csv("data-raw/FullSurveyorInteract_Modifiers.csv")
 
 combine_dictionary <- function(id_d, beh_d, mod_d, country, year) {
   id_d <- id_d %>% mutate(type = "identity", country = country, year = year)
-  beh_d <- beh_d %>% mutate(type = "behavior", county = country, year = year)
+  beh_d <- beh_d %>% mutate(type = "behavior", country = country, year = year)
   mod_d <- mod_d %>% mutate(type = "modifier", country = country, year = year)
 
   full_dictionary <- bind_rows(id_d, beh_d, mod_d)
@@ -16,4 +16,4 @@ combine_dictionary <- function(id_d, beh_d, mod_d, country, year) {
 
 us_2015_full <- combine_dictionary(us_2015_id, us_2015_beh, us_2015_mod, "US", 2015)
 
-usethis::use_data(DATASET, overwrite = TRUE)
+usethis::use_data(us_2015_full, overwrite = TRUE)
