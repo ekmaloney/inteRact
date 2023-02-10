7938c7ee00e57fa8c4e957c3093f3f0e9277052c
Emily Maloney <45981342+ekmaloney@users.noreply.github.com>
2023-02-10 16:11:34 -0500

    adding deflection tests for canada, germany, and egypt

--- tests/testthat/test-get_deflection.R
+++ tests/testthat/test-get_deflection.R
@@ -1,5 +1,5 @@
 
-test_that("get deflection replicates Interact", {
+test_that("get deflection replicates Interact - NC 1978", {
 
   d <- tibble(actor = "brute", behavior = "work", object = "cook")
 
@@ -12,4 +12,37 @@ test_that("get deflection replicates Interact", {
 
   d <- round(d, digits = 1)
   expect_equal(d, 1.9)
+})
+
+
+test_that("get deflection replicates Interact - Canada", {
+
+  d <- tibble(actor = "academic", behavior = "accommodate", object = "accomplice")
+
+  d <- reshape_events_df(df = d,
+                         df_format = "wide",
+                         dictionary_key = "ontario2001", dictionary_gender = "male")
+
+  d <- get_deflection(d = d, equation_key = "canada1985",
+                      equation_gender = "male")
+
+  d <- round(d, digits = 1)
+  expect_equal(d, 0.6)
+})
+
+test_that("get deflection replicates Interact - Egypt", {
+
+  d <- tibble(actor = "abortionist", behavior = "abuse", object = "adolescent")
+
+  d <- reshape_events_df(df = d,
+                         df_format = "wide",
+                         dictionary_key = "egypt2015",
+                         dictionary_gender = "all")
+
+  d <- get_deflection(d = d,
+                      equation_key = "egypt2014",
+                      equation_gender = "all")
+
+  d <- round(d, digits = 1)
+  expect_equal(d, 8.5)
 })
